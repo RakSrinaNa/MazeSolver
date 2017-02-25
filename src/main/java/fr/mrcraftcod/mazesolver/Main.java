0aba3ec31d39d5c9ecf2ccf20e26ced5349bac42
Thomas Couchoud <zerderr@gmail.com>
2017-02-25 09:31:38 +0100

    First commit

---
+++ src/main/java/fr/mrcraftcod/mazesolver/Main.java
@@ -0,0 +1,15 @@
+package fr.mrcraftcod.mazesolver;
+
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 25/02/2017.
+ *
+ * @author Thomas Couchoud
+ * @since 2017-02-25
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
